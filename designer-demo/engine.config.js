b20971acea7f2ced330af86d07496e9f7b5d0a5a
qguhxdsj@megazr.com <qguhxdsj@megazr.com>
2026-01-15 18:57:42 +0800

    修改2.9x配置

--- designer-demo/engine.config.js
+++ designer-demo/engine.config.js
@@ -1,7 +1,7 @@
 export default {
   id: 'engine.config',
   theme: 'light',
-  material: ['/mock/bundle.json'],
+  material: ['http://127.0.0.1:8090/mock/bundle.json'],
   scripts: [],
   styles: [],
   // 是否开启 TailWindCSS 特性
